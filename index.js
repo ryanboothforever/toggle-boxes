11b72f8488281b8bcc9957cc356f02609f7114c8
Marcus Haddon <marcus.haddon@nike.com>
2020-10-01 13:05:27 -0700

    make it work for any number of boxes

--- index.js
+++ index.js
@@ -1,29 +1,19 @@
-let box1 = document.querySelector(".box1");
-let box2 = document.querySelector(".box2");
-let box3 = document.querySelector(".box3");
+const allBoxesResult = document.querySelectorAll('.shape');
+// We have to convert this to an array before we can use array methods on it.
+// I wouldn't have expected you to know this, and I only remembered because I got an error at first.
+const allBoxes = Array.from(allBoxesResult);
 
-box1.addEventListener("click", selectSquare);
+for (const box of allBoxes) {
+  box.addEventListener('click', function(event) {
+    const clickedBox = event.target;
+    const notClickedBoxes = allBoxes.filter(function(box) {
+      return box.id !== clickedBox.id;
+    });
 
-function selectSquare() {
-  box1.classList.toggle("pressed");
-    if(box1.classList.contains("pressed")){
-      box2.classList.remove("pressed");
-      box3.classList.remove("pressed");
+    for (const box of notClickedBoxes) {
+      box.classList.remove('pressed');
     }
-};
 
-box2.addEventListener("click", event => {
-  box2.classList.toggle("pressed");
-  if (box2.classList.contains("pressed")){
-    box1.classList.remove("pressed");
-    box3.classList.remove("pressed");
-  }
-});
-
-box3.addEventListener("click", event => {
-  box3.classList.toggle("pressed");
-  if(box3.classList.contains("pressed")){
-      box2.classList.remove("pressed");
-      box1.classList.remove("pressed");
-  }
-});
+    clickedBox.classList.add('pressed');
+  })
+}
